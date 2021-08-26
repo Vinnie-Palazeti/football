32815fd1e33de4c2daf07c6ce8884e26e5f96796
Vinnie Palazeti <vinnie.palazeti@gmail.com>
2021-08-25 22:57:49 -0400

    removing data files

--- play_pred/data/scrape.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash                
-
-for year in {2013..2020}; do
-  echo downloading $year play-by-play data ...
-  curl -o nflsavant_${year}.csv http://www.nflsavant.com/pbp_data.php?year=$year
-done
